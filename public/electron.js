d6a965425fb966cc0af709b583d3d517779be308
joshuareid <2001josh@gmail.com>
2025-10-01 12:12:51 -0400

    first commit

---
+++ public/electron.js
@@ -0,0 +1,45 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+// Simple development check instead of electron-is-dev
+const isDev = process.env.NODE_ENV === 'development' || process.defaultApp || /node_modules[\\/]electron[\\/]/.test(process.execPath);
+
+function createWindow() {
+    // Create the browser window
+    const mainWindow = new BrowserWindow({
+        width: 1200,
+        height: 800,
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false
+        }
+    });
+
+    // Load the app
+    const startUrl = isDev
+        ? 'http://localhost:3000'
+        : `file://${path.join(__dirname, '../build/index.html')}`;
+
+    mainWindow.loadURL(startUrl);
+
+    // Open DevTools in development
+    if (isDev) {
+        mainWindow.webContents.openDevTools();
+    }
+}
+
+// This method will be called when Electron has finished initialization
+app.whenReady().then(createWindow);
+
+// Quit when all windows are closed
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
+
+app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow();
+    }
+});
